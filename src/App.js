d75258bffb1313678e7c0318ff003bcd6734715e
zerolake <zerolake@tuta.io>
2021-05-15 18:16:59 -0400

    added basic file structure and App content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import React, {useState, useEffect} from 'react'
+import Header from './Components/Header.js'
+import CardContainer from './Components/CardContainer.js'
+import Footer from './Components/Footer.js'
+
 function App() {
+  const [score, setScore] = useState(0)
+  const [highScore, setHighScore] = useState(0)
+
+  const addScore = () => {
+    setScore(score + 1)
+  }
+
+  const gameOver = () => {
+    setScore(0)
+  }
+
+  useEffect(() => {
+    if (score > highScore) {
+      setHighScore(score)
+    }
+  },[score, highScore])
+
   return (
     <div className="App">
-      <h1>Hello!</h1>
+      <Header score={score} highScore={highScore}/>
+      <CardContainer addScore={addScore} gameOver={gameOver}/>
+      <Footer/>
     </div>
   )
 }
