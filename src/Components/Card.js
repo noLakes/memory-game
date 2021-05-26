8eb37ed443c8ea2ab1232e9b8bde3974bc64a15d
zerolake <zerolake@tuta.io>
2021-05-25 21:51:27 -0400

    added background images for animal cards

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 
 function Card(props) {
   const [clicked, setClicked] = useState(false)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const resetPicked = () => {
@@ -15,6 +16,10 @@ function Card(props) {
     }
   },[props.reset, clicked])
 
+  useEffect(() => {
+
+  })
+
   const updateCard = (e) => {
     clicked ? props.handleClick(true) : props.handleClick(false)
 
@@ -22,7 +27,11 @@ function Card(props) {
   }
   
   return(
-    <div 
+    <div
+      style={{
+        backgroundImage: `url(${props.image})`,
+        //backgroundImage: `url(${props.image.replace('-400x300', '')})`,
+      }}
       id={props.id}
       className='card'
       onClick={updateCard}
