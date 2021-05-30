9d0c1c9c6ee1d901b5cc2d09632a8621b64c8201
zerolake <zerolake@tuta.io>
2021-05-30 16:42:17 -0400

    added options to select card count

--- App.js
+++ App.js
@@ -6,10 +6,15 @@ import Footer from './Components/Footer.js'
 function App() {
   const [score, setScore] = useState(0)
   const [highScore, setHighScore] = useState(0)
+  const [cardCount, setCardCount] = useState(12)
 
   const addScore = () => {
     console.log("adding score.")
     setScore(score + 1)
+  }
+
+  const handleCardCount = (e) => {
+    setCardCount(e.target.value)
   }
 
   const gameOver = () => {
@@ -25,8 +30,8 @@ function App() {
 
   return (
     <div className="main-container">
-      <Header score={score} highScore={highScore}/>
-      <CardContainer addScore={addScore} gameOver={gameOver}/>
+      <Header score={score} highScore={highScore} cardCount={cardCount} handleCardCount={handleCardCount}/>
+      <CardContainer addScore={addScore} gameOver={gameOver} cardCount={cardCount}/>
       <Footer/>
     </div>
   )
--- Components/Header.js
+++ Components/Header.js
@@ -3,6 +3,16 @@ function Header(props) {
   return (
     <div className='header-container'>
       <h1>Memory Game!</h1>
+      <h1>Cards:
+        <select value={props.cardCount} onChange={props.handleCardCount}>
+          <option value='12'>12</option>
+          <option value='16'>16</option>
+          <option value='20'>20</option>
+          <option value='24'>24</option>
+          <option value='28'>28</option>
+          <option value='32'>32</option>
+        </select>
+      </h1>
       <h1 className='score'>
         Score:
         <span> {props.score}</span>
